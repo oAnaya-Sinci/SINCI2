34d2e93d69d765483eca21d74c6532443446afff
Carlos Anaya <a_naya616@hotmail.com>
2022-02-22 13:24:16 -0600

    Changues of the proyect to the format 2

--- public/js/calendar/fullcalendar.js
+++ public/js/calendar/fullcalendar.js
@@ -75,7 +75,7 @@
 
         monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
         monthNamesShort: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'],
-        dayNames: ['Domingo', 'Lunes', 'Martes', 'miercoles', 'Jueves', 'Viernes', 'Sabado'],
+        dayNames: ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado'],
         dayNamesShort: ['Dom', 'Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sab'],
         buttonText: {
             prev: "<span class='fc-text-arrow'>&lsaquo;</span>",

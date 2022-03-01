22d204a0bd6188bb63aeb0ac3720c4eb7ec71581
Carlos Anaya <a_naya616@hotmail.com>
2022-03-01 17:45:03 -0600

    Cambios por error de routas en servidor, se añadio ruta para hacer pruebas 2

--- routes/web.php
+++ routes/web.php
@@ -54,10 +54,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/testingRoutes', function(){ return "Omar Anaya"; });
 
 // This route only shows the PHP info of the server
-Route::get('infoPHP', function(){ return view('info'); });
+Route::get('/infoPHP', function(){ return view('info'); });
 
 Route::get('/', function(){ return view('authenticate/loginUser'); });
 
-Route::get('dashboard', function(){ $titulo = "DASHBOARD"; return view('dashboard/main', compact('titulo')); });
+Route::get('/dashboard', function(){ $titulo = "DASHBOARD"; return view('dashboard/main', compact('titulo')); });
 
-Route::get('bitacoras/main', function(){ $titulo = "REGISTRO DE BITÁCORAS"; return view('bitacoras/main', compact('titulo')); });
+Route::get('/bitacoras/main', function(){ $titulo = "REGISTRO DE BITÁCORAS"; return view('bitacoras/main', compact('titulo')); });

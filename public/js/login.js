bf8489fefbd1806a50ee5a61ef0daeaff99b1a0e
Carlos Anaya <a_naya616@hotmail.com>
2022-03-10 12:25:13 -0600

    Changues to upload the project at production server v1.7

--- public/js/login.js
+++ public/js/login.js
@@ -12,6 +12,8 @@ var timeOut;
 
 $(document).ready(function() {
 
+    $('.input-group.input-group-outline').removeClass('is-filled');
+
     fetch(urlData + "/openconn");
     IsLogedIn();
     outLoader();

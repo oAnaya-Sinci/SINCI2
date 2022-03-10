b594da65b0677b8a5d7ba2bf5dd4b64cda96c792
Carlos Anaya <a_naya616@hotmail.com>
2022-03-10 12:14:58 -0600

    Changues to upload the project at production server v1.4

--- resources/views/authenticate/loginUser.blade.php
+++ resources/views/authenticate/loginUser.blade.php
@@ -11,8 +11,8 @@
         <link rel="icon" type="image/png" href="/img/sinci.ico" sizes="32x32">
         <title>{{ config('app.name', 'SAS SINCI') }}</title>
         
-        <!-- Font Awesome Icons -->
-        <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+        <!-- Material Icons -->
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
         <!-- CSS Files -->
         <link id="pagestyle" href="/css/material-dashboard.css?v=3.0.0" rel="stylesheet" />
 

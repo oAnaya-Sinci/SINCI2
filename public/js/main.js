69432ea0d6d76f41a1525b59948a214d3c2ad6e8
Carlos Anaya <a_naya616@hotmail.com>
2022-06-02 12:10:12 -0500

    Se realizo cambio a la url para que trabaje con la url del servidor

--- public/js/main.js
+++ public/js/main.js
@@ -3,15 +3,15 @@
 // HTTP
 // var urlData = "http://10.10.100.34:1880"; // PRODUCTION SERVER WITHOUT SECURE PROTOCOL 80
 
-// var URL to my Local UBUTBU Server for the update
-// var urlData = "https://192.168.0.102:1880";
-var urlData = "http://localhost:1880";
-// var urlData = "https://10.10.100.34:1880";
-
 var currenUrl = window.location.href.split("/")[2];
 currenUrl = currenUrl.split(":");
 
-// var urlData = "https://" + currenUrl[0] + ":1880";
+var urlData = "https://" + currenUrl[0] + ":1880";
+
+// var URL to my Local UBUTBU Server for the update
+// var urlData = "https://192.168.0.102:1880";
+// var urlData = "http://localhost:1880";
+// var urlData = "https://10.10.100.34:1880";
 
 var timeOut;
 

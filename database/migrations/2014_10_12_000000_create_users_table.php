33051f9a37d7750330400e66220c3ef8a22af96e
arturo_flores <aflores@sinci.com>
2023-10-31 15:34:06 -0600

    se agrego is_admin

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('email_notifi')->nullable();
             $table->string('telegram_notifi')->nullable();
-            $table->string('chat_id');
+            $table->string('chat_id')->nullable();
+            $table->string('is_admin')->default('0');
             $table->string('days')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password')->nullable();

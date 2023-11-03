a02cf79873f309231ea25e0935dce84358975ae0
arturo_flores <aflores@sinci.com>
2023-11-03 16:16:29 -0600

    filtro de usuarios en reporte

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -2,16 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 use App\Models\Department;
 use App\Models\User;
 use App\Models\Date;
 
+
 class ReportController extends Controller
 {
-    public function index(){
-
-        $users = User::all();
+    public function index()
+    {
+        
+        $users = User::whereRelation('positions', 'id', '=', 4)->get();
         $departments = Department::all()->pluck('name', 'id');
         $titulo = 'REPORTES';
         
@@ -20,15 +23,19 @@ class ReportController extends Controller
     }
 
 
-    public function filter(Request $request){
+    public function filter(Request $request)
+    {
 
         $departments = Department::all()->pluck('name', 'id');
         $titulo = 'REPORTES';
         $date = Date::all()->value('setting_date');
- 
+
         $users = User::whereHas('departments', function ($query) use ($request) {
             $query->where('id', $request->input('department'));
+        })->whereHas('position_user', function($query) {
+            $query->where('position_id','=', 4);
         })->get();
+      
  
         return view('reports.index', compact('users', 'departments', 'titulo', 'date'));
     }

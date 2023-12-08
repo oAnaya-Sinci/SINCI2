81a52c5ea1489ee0f96670e401ac3a4897431960
arturo_flores <aflores@sinci.com>
2023-12-08 11:42:48 -0600

    se quito ing de la lista

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,7 +16,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        $users = User::all();
+        $users = User::orderBy('name')->get();
         $titulo = 'ADMINISTRACIÓN DE USUARIOS';
 
         return view('users.index', compact('users', 'titulo'));
